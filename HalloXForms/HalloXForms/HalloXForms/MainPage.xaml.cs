b30e4d735d98dd9ca48c5439c97646102a941136
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-02 14:42:24 +0200

    Steuerelemente

--- HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
+++ HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
@@ -13,6 +13,7 @@ namespace HalloXForms
         {
             InitializeComponent();
         }
+        private string aktuelleFarbe;
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
@@ -28,7 +29,39 @@ namespace HalloXForms
             // 3) DisplayActionSheet mit mehreren Antworten ("Ja/Nein")
             string farbe = await DisplayActionSheet("Wähle deine Lieblingsfarbe", null, null, "Rot", "Gelb", "Grün", "Blau", "Orange");
             await DisplayAlert("Deine Farbe war:", farbe, "Yay");
+            aktuelleFarbe = farbe;
+        }
 
+        private void ButtonLogin_Clicked(object sender, EventArgs e)
+        {
+            if(entryPasswort.Text == "geheim")
+            {
+                DisplayAlert("Login", "Login erfolgreich", "Yay");
+            }
+        }
+
+        private async void ButtonZeit_Clicked(object sender, EventArgs e)
+        {
+            var aktuelleZeit = timePicker.Time;
+            var aktuellesDatum = datePicker.Date;
+
+            await DisplayAlert("Zeit", aktuelleZeit.ToString(), "OK");
+            await DisplayAlert("Datum", aktuellesDatum.ToString(), "OK");
+
+            // StringInterpolation C# 6
+            string text1 = "Hallo";
+            string text2 = "Welt";
+            string text3 = "!";
+
+            int zahl1 = 5;
+            int zahl2 = 10;
+
+            string ausgabe = text1 + " " + text2 + " " + text3;
+            // "Die Summe von " + zahl1 + " und " + zahl2 + " ist " + ergebnis;
+
+            string ausgabe2 = $"{text1} {text2}";
+
+            var x = $"Die Summe von {zahl1} und {zahl2} ist {zahl1 + zahl2}";
         }
     }
 }
