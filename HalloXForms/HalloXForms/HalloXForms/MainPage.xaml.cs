3e6bac210692cc281a00be53ced6cbe3bbbb593b
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-02 12:13:30 +0200

    Nachrichten - Varianten

--- HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
+++ HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
@@ -13,5 +13,22 @@ namespace HalloXForms
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            // 3 Arten für Nachrichten:
+
+            // 1) DisplayAlert mit einer Antwort
+            // await DisplayAlert("Meine erste Nachricht", "Hallo Welt", "OK");
+
+            // 2) DisplayAlert mit zwei Antworten ("Ja/Nein")
+            //bool antwort = await DisplayAlert("Meine Frage", "Wollen wir Mittagspause machen ?", "Ja","Nein");
+            //await DisplayAlert("Deine Antwort war:", antwort.ToString(), "OK");
+
+            // 3) DisplayActionSheet mit mehreren Antworten ("Ja/Nein")
+            string farbe = await DisplayActionSheet("Wähle deine Lieblingsfarbe", null, null, "Rot", "Gelb", "Grün", "Blau", "Orange");
+            await DisplayAlert("Deine Farbe war:", farbe, "Yay");
+
+        }
     }
 }
