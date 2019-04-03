f6766eebb498bfb04e1862b46ae5026b0a125a57
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-03 09:54:53 +0200

    Variante mit DynamicResource

--- MittwochEinstieg/MittwochEinstieg/MittwochEinstieg/MainPage.xaml.cs
+++ MittwochEinstieg/MittwochEinstieg/MittwochEinstieg/MainPage.xaml.cs
@@ -16,10 +16,16 @@ namespace MittwochEinstieg
 
         private void SwitchDarkMode_Toggled(object sender, ToggledEventArgs e)
         {
+            // StaticResource: Element ist editierbar, aber darf nicht ausgetauscht werden
+
+            // DynamicResource: Erlaubt das Austauschen der Elemente in der Ressource
+            // Idee: Button verweist mit einem Binding auf die Ressource "currentButtonStyle"
+            // Im Code: this["currentButtonStyle"]= this.Ressource["lightMode"]
+
             if (e.Value == true)
-                buttonLogin.Style = (Style)this.Resources["buttonDarkTheme"];
+                this.Resources["currentButtonStyle"] = this.Resources["buttonDarkTheme"];
             else
-                buttonLogin.Style = this.Resources["buttonLightTheme"] as Style;
+                this.Resources["currentButtonStyle"] = this.Resources["buttonLightTheme"];
         }
     }
 }
