58122f4739a3e1927ebd2fa08c6e52699411a42b
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-03 12:35:43 +0200

    Images - Varianten

--- Image_Demo/Image_Demo/Image_Demo/MainPage.xaml.cs
+++ Image_Demo/Image_Demo/Image_Demo/MainPage.xaml.cs
@@ -12,6 +12,20 @@ namespace Image_Demo
         public MainPage()
         {
             InitializeComponent();
+
+            // Variante 1
+            //var source = (UriImageSource)ImageSource.FromUri(new Uri("asdasdasda"));
+            //source.CachingEnabled = true; // Standardfall ist True
+            //source.CacheValidity = TimeSpan.FromMinutes(10); // Standardfall ist 24h
+
+            //image1.Source = source;
+
+            // Variante 2:
+            // image1.Source = ImageSource.FromFile("katze.jpg");
+
+            // Variante 3:
+            // Automatisch generierte Resourcen-ID: Projektname.Ordnername.Dateiname.Extension
+            // image1.Source = ImageSource.FromResource("Image_Demo.Images.hund.jpg");
         }
     }
 }
