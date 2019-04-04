df119e00299877b78e5134bcab5bd4a3741e56c9
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-04 12:13:32 +0200

    Definition der Daten erstellt

---
+++ MVVM_Beispiel/MVVM_Beispiel/MVVM_Beispiel/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_Beispiel.Models
+{
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string GanzerName => $"{Vorname} {Nachname}";
+        // Kurzschreibweise für:
+        //{
+        //    get
+        //    {
+        //        return $"{Vorname} {Nachname}";
+        //    }
+        //}
+
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+    }
+}
