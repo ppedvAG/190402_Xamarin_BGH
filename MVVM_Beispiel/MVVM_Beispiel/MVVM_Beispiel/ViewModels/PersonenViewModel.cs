f2aac3125ec39d37b5bec68e99c039b1737f2b28
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-04 12:20:52 +0200

    Basisversion vom ViewModel erstellt

---
+++ MVVM_Beispiel/MVVM_Beispiel/MVVM_Beispiel/ViewModels/PersonenViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MVVM_Beispiel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MVVM_Beispiel.ViewModels
+{
+    public class PersonenViewModel
+    {
+        public PersonenViewModel()
+        {
+            service = new PersonenService();
+            LadePersonenCommand = new Command(LadePersonen);
+        }
+        private readonly PersonenService service;
+
+
+
+        // Öffentlich für die View sichtbar:
+        public List<Person> Personenliste { get; set; }
+        public Command LadePersonenCommand { get; set; } // Ersatz für Button.Clicked - Events
+        private void LadePersonen(object obj)
+        {
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
