8cb4a95fa9b7250637b1a83ea94e0563ee75f50a
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-05 15:33:33 +0200

    MessagingCenter - Beispiel mit Label und Slider

---
+++ MessagingCenter_Demo/MessagingCenter_Demo/MessagingCenter_Demo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessagingCenter_Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Subscribe
+            MessagingCenter.Subscribe<Slider, double>(this, "ValueChanged", LabelÄndern);
+
+            // Unsubscribe:
+            // MessagingCenter.Unsubscribe<Slider,double>(this, "ValueChanged");
+        }
+
+        private void LabelÄndern(Slider arg1, double arg2)
+        {
+            labelWert.Text = arg2.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Seite2()); // Es wird jedes Mal eine neue Seite2-Instanz erstellt
+        }
+    }
+}
---
+++ MessagingCenter_Demo/MessagingCenter_Demo/MessagingCenter_Demo/Seite2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessagingCenter_Demo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Seite2 : ContentPage
+	{
+		public Seite2 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void SliderWert_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // Publisher -> Schickt "Benachrichtigung" an alle Subscriber, die sich für die Nachricht "ValueChanged" interessieren
+            MessagingCenter.Send(sliderWert, "ValueChanged", sliderWert.Value); 
+        }
+    }
+}
