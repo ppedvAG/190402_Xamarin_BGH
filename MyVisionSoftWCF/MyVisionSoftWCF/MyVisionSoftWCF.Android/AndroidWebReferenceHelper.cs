006a97f5824f3eadc79827b7847153c1c278abcb
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-05 12:21:23 +0200

    AndroidWebrefrenceHelper erstellt, der sich das DataSet holt und an das .NET Standard-Projekt weiterreicht

---
+++ MyVisionSoftWCF/MyVisionSoftWCF/MyVisionSoftWCF.Android/AndroidWebReferenceHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MyVisionSoftWCF.Droid.AndroidWebReferenceHelper))]
+namespace MyVisionSoftWCF.Droid
+{
+    public class AndroidWebReferenceHelper : IWebReferenceHelper
+    {
+        public object GetTestDataSet()
+        {
+            var service = new MyVisionSoft.Logic.WebReference.Service();
+            var dataSet = service.TestFileList();
+            return dataSet;
+        }
+    }
+}
