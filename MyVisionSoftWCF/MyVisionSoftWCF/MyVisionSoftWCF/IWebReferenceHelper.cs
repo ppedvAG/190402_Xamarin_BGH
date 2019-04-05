b5f5e67fd86e7d68515a8016b2f5613cac4724a1
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-05 12:18:13 +0200

    Schnittstelle für den DependencyService erstellen

---
+++ MyVisionSoftWCF/MyVisionSoftWCF/MyVisionSoftWCF/IWebReferenceHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVisionSoftWCF
+{
+    public class IWebReferenceHelper
+    {
+        object GetTestDataSet();
+    }
+}
